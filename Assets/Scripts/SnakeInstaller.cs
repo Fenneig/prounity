bf8d89f7b0aad9c622dbfa2a39a05fefe09c6c2d
Fenneig <ninetailedkot92@gmail.com>
2026-06-15 17:11:21 +0300

    init task

---
+++ Assets/Scripts/SnakeInstaller.cs
@@ -0,0 +1,25 @@
+﻿using Modules;
+using SnakeGame;
+using UnityEngine;
+using Zenject;
+
+public class SnakeInstaller : MonoInstaller
+{
+    [SerializeField] private WorldBounds _worldBounds;
+    [SerializeField] private Snake _snake;
+    [SerializeField] private Coin _coinPrefab;
+    [SerializeField] private Transform _coinContainer;
+    
+    public override void InstallBindings()
+    {
+        Container.Bind<ISnake>().To<Snake>().FromInstance(_snake);
+        
+        Container.Bind<IWorldBounds>().To<WorldBounds>().FromInstance(_worldBounds);
+        
+        Container
+            .BindMemoryPool<Coin, CoinPool>()
+            .FromComponentInNewPrefab(_coinPrefab)
+            .UnderTransform(_coinContainer)
+            .AsSingle();
+    }
+}
