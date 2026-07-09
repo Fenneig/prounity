6374504d7dcf27a3fe830f26f93d457ef8909eb7
Fenneig <ninetailedkot92@gmail.com>
2026-07-09 16:45:57 +0300

    init commit

---
+++ Assets/Game/Scripts/GameObjects/Components/Physics/TriggerComponent.cs
@@ -0,0 +1,15 @@
+using System;
+using UnityEngine;
+
+namespace Game
+{
+    public sealed class TriggerComponent : MonoBehaviour
+    {
+        public event Action<Collider2D> OnEntered;
+        public event Action<Collider2D> OnExited;
+
+        private void OnTriggerEnter2D(Collider2D col) => this.OnEntered?.Invoke(col);
+
+        private void OnTriggerExit2D(Collider2D col) => this.OnExited?.Invoke(col);
+    }
+}
